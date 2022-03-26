ca45de7d80f83b0d3e991ea632e159ddf96a3d2a
ZxyKira <danny41553@gmail.com>
2022-03-26 10:39:42 +0800

    [add] project init

---
+++ .keil/make-package.bat
@@ -0,0 +1,2 @@
+java -Dfile.encoding=UTF-8 -jar packet-tool.jar package ./../src/main/driver/ DRIVER_
+pause
---
+++ src/main/driver/package-info.h
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2020 ZxyKira
+ * All rights reserved.
+ * 
+ * SPDX-License-Identifier: MIT
+ */
+#ifndef DRIVER_E28AC1FD_AF0A_4768_A5E0_B77CCEBDC476
+#define DRIVER_E28AC1FD_AF0A_4768_A5E0_B77CCEBDC476
+
+/* ****************************************************************************************
+ * Include folder
+ */
+
+/* ****************************************************************************************
+ * Include
+ */
+
+/* ****************************************************************************************
+ * End of file
+ */
+
+#endif /* DRIVER_E28AC1FD_AF0A_4768_A5E0_B77CCEBDC476 */
